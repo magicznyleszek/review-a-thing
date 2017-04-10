3a37427adcb0821ee3c983604fb9328f4d1ced51
Leszek Pietrzak <smutnyleszek@gmail.com>
2017-04-10 11:20:36 +0200

    stepsMenuCtrl final test

--- test/stepsModule/stepsMenuCtrlTest.js
+++ test/stepsModule/stepsMenuCtrlTest.js
@@ -1,15 +1,26 @@
 describe('stepsMenuCtrl', () => {
     let stepsMenuCtrl = null;
+    let appActions = null;
+    let appStore = null;
 
     beforeEach(() => {
         module('testAppModule');
         module('stepsModule');
         inject(($injector, $controller) => {
             stepsMenuCtrl = $controller('stepsMenuCtrl');
+            appActions = $injector.get('appActions');
+            appStore = $injector.get('appStore');
         });
     });
 
     it('should get initial steps state', () => {
         expect(stepsMenuCtrl.options.length > 0).toBeTruthy();
+    });
+
+    it('should change step when option callback is triggered', () => {
+        appActions.setCurrentStepId('summary');
+        stepsMenuCtrl.options[0].show();
+        const state = appStore.getState();
+        expect(state.currentStepId).toBe('review');
     });
 });
